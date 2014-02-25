b9768e42f0a90272882360f7facb705fc0c6d1a2
grahamcrackers <grogers12487@gmail.com>
2014-02-25 17:45:45 -0600

    Defining a Method on the Scope

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
-function FirstCtrl($scope) {
+"use strict";
 
+var myApp = angular.module('myApp', []);
+myApp.factory('Data', function() {
+    return {message:"I'm data from a service"}
+
+})
+
+
+function FirstCtrl($scope, Data) {
+ $scope.data = Data;
 }
 
-function SecondCtrl($scope) {
+function SecondCtrl($scope, Data) {
+ $scope.data = Data;
 
+    $scope.reversedMessage = function(message) {
+        return message.split("").reverse("").join("");
+    }
 }
